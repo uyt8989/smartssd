63d780b13f88d25b7b3905eb491668a07564a9d5
uyt8989 <87462266+uyt8989@users.noreply.github.com>
2022-02-22 15:17:19 +0900

    Create Pagerank.cpp

---
+++ pagerank/Pagerank.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <Eigen/Dense>
+
+using namespace std;
+using namespace Eigen;
+
+const int iterations = 1000;
+
+int main()
+{
+    MatrixXd M = MatrixXd(5, 5);
+    VectorXd v = VectorXd(5);
+    float d = 0.85;
+
+    M << 0, 0, 0, 0, 1,
+    0.5, 0, 0, 0, 0,
+    0.5, 0, 0, 0, 0,
+    0, 1, 0.5, 0, 0,
+    0, 0, 0.5, 1, 0;
+
+    srand(time(0));
+    int sum = 0;
+    for(int i = 0; i < M.cols(); i++){
+        v(i) = rand() % 1000;
+        sum += v(i);
+    }
+
+    for(int i = 0; i < M.cols(); i++){
+        v(i) = v(i) / sum;
+    }
+
+    MatrixXd temp = MatrixXd(5, 5);
+    float dd = (1 - d) / M.cols();
+    for(int i = 0; i < M.rows(); i++) {
+        for(int j = 0; j < M.cols(); j++) {
+            temp(i, j) = dd;
+        }
+    }
+
+    MatrixXd M_hat = d * M + temp;
+    for(int i = 0; i < iterations; i++) {
+        v = M_hat * v;
+    }
+    cout << v << endl;
+   
+    return 0;
+}
